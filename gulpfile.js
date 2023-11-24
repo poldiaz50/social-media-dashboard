7fab35188116fd7d89e18ab02e73332135fddfe8
poldiaz50 <108167418+poldiaz50@users.noreply.github.com>
2023-11-24 00:38:34 -0500

    Set up Gulp workflow

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+//modulos iniciales
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const babel = require('gulp-babel');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
+
+//use dart-sass for @use
+sass.compiler = require('dart-sass');
+
+//sass tasks
+function scssTask() {
+    return src('app/scss/style.scss', { sourcemaps: true })
+        .pipe(sass())
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(dest('dist', { sourcemaps: '.' }))
+}
+
+//javascript Task
+function jsTask() {
+    return src('app/js/script.js', { sourcemaps: true })
+        .pipe(babel({ presets: ['@babel/preset-env'] }))
+        .pipe(terser())
+        .pipe(dest('dist', { sourcemaps: '.' }))
+}
+
+//browser sync
+function browserSyncServer(cb) {
+    browsersync.init({
+        server: {
+            baseDir: '.',
+        },
+        notify: {
+            styles: {
+                top: 'auto',
+                bottom: '0',
+            },
+        },
+    });
+    cb(); 
+}
+function browserSyncReload (cb){
+    browsersync.reload();
+    cb();
+}
+
+//wathc Task
+function watchTask () {
+    watch('*.html', browserSyncReload);
+    watch(
+        ['app/scss/**/*.scss', 'app/**/*.js'],
+        series(scssTask, jsTask, browserSyncReload)
+    );
+}
+
+//default gup Task
+exports.default = series(scssTask, jsTask, browserSyncServer, watchTask);
